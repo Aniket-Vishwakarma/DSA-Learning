6fa76b70efa4b1e979334ff599fe4e7ede0a4346
Aniket-Vishwakarma <vishwakarmaaniket790@gmail.com>
2025-08-10 11:58:03 +0530

    second commit inside dynammic programming in max_k_stocks

---
+++ Dynammic-Programming/max_k_stocks.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int solve(int buy  , int count , int k , int index , int n , int *a , vector<vector<vector<int>>> &dp){
+        if(count == k) return 0;
+        if(index == n) return 0;
+        if(dp[index][count][buy] != -1) return dp[index][count][buy];
+        int ans1 = 0 , ans2 = 0;
+        if(buy){
+            ans1 = -a[index] + solve(buy - 1  , count , k , index + 1 , n , a , dp);
+            ans2 = solve(buy  , count , k , index + 1 , n , a , dp);
+        }
+        else{
+            ans1 = a[index] + solve(1 - buy  , count + 1 , k , index + 1 , n , a , dp);
+            ans2 = solve(buy , count , k , index + 1 , n , a , dp);
+        }
+        return dp[index][count][buy] = max(ans1 , ans2);
+    }
+
+    int maxProfit(int K, int N, int A[]) {
+        vector<vector<vector<int>>> dp(N + 1 , vector<vector<int>>(K , vector<int>(2 , -1)));
+        return solve(1 , 0 , K , 0 , N , A , dp);
+    }
+};
